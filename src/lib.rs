520a87bee8c6df5c70ee5d99a1d18163c376168a
jerry73204 <jerry73204@gmail.com>
2021-02-02 07:58:18 +0800

    Remove MapPerm trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ pub mod prelude {
     pub use super::apply::PermApply;
     pub use super::from_indices::PermFromIndices;
     pub use super::from_sorting::PermFromSorting;
-    pub use super::map_perm::MapPerm;
     pub use super::perm_trait::Permutation;
     pub use super::product::PermProduct;
 }
@@ -13,7 +12,6 @@ mod apply;
 mod common;
 mod from_indices;
 mod from_sorting;
-mod map_perm;
 mod perm_trait;
 mod perm_type;
 mod product;
@@ -23,7 +21,6 @@ pub mod size;
 pub use apply::*;
 pub use from_indices::*;
 pub use from_sorting::*;
-pub use map_perm::*;
 pub use perm_trait::*;
 pub use perm_type::*;
 pub use product::*;
