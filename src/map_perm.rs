674e4b625d7c7c2d8bcb0cbfc3f3124175254cc2
jerry73204 <jerry73204@gmail.com>
2021-02-02 00:23:23 +0800

    Implement MapPerm for Vec

--- src/map_perm.rs
+++ src/map_perm.rs
@@ -12,14 +12,36 @@ where
         S: PermSize;
 }
 
-impl<T> MapPerm for &mut [T] {
-    type Error = &'static str;
+mod without_std {
+    use super::*;
 
-    fn map_perm<S>(self, perm: Perm<S>) -> Result<Self, Self::Error>
-    where
-        Perm<S>: Permutation,
-        S: PermSize,
-    {
-        perm.apply(self)
+    impl<T> MapPerm for &mut [T] {
+        type Error = &'static str;
+
+        fn map_perm<S>(self, perm: Perm<S>) -> Result<Self, Self::Error>
+        where
+            Perm<S>: Permutation,
+            S: PermSize,
+        {
+            perm.apply(self)
+        }
+    }
+}
+
+#[cfg(not(feature = "no_std"))]
+mod with_std {
+    use super::*;
+
+    impl<T> MapPerm for &mut Vec<T> {
+        type Error = &'static str;
+
+        fn map_perm<S>(self, perm: Perm<S>) -> Result<Self, Self::Error>
+        where
+            Perm<S>: Permutation,
+            S: PermSize,
+        {
+            perm.apply(self.as_mut_slice())?;
+            Ok(self)
+        }
     }
 }
